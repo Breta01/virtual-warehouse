eec4539c5a9ea84b228ae57005de16f0eee4ca69
Breta01 <breta.hajek@gmail.com>
2021-04-08 21:06:42 +0200

    Test release v0.0.1
    Fix poetry, github action, cli runnable
    
    Fix poetry packaging

--- virtual_warehouse/__main__.py
+++ virtual_warehouse/__main__.py
@@ -1,7 +1,11 @@
-if __name__ == "__main__":
-
+def main():
+    """Run application. Main entry point from command line."""
     import sys
 
     from virtual_warehouse import app
 
     sys.exit(app.run())
+
+
+if __name__ == "__main__":
+    main()
